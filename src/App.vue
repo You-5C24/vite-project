fb745b6e396e4b3e5b7dfc8f0c8051528b39b061
5C24 <you_5c24@163.com>
2022-04-18 14:46:06 +0800

    feat: css工程化

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Header />
+  <div class="p-20px text-center">
+    <h1 class="font-bold text-2xl mb-2">
+      vite version: {{ devDependencies.vite }}
+    </h1>
+  </div>
+  <button
+    bg="blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600"
+    text="sm white"
+    font="mono light"
+    p="y-2 x-4"
+    border="2 rounded blue-200"
+  >
+    Button
+  </button>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script setup lang="ts">
+import Header from "./components/Header/index.vue";
+import { devDependencies } from "../package.json";
+</script>
+
+<style></style>
---
+++ src/components/Header/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex-c">
+    <p :class="styles.header">This is Header</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "HeaderIndex",
+};
+</script>
+
+<script setup lang="ts">
+import styles from "./index.module.scss";
+</script>
+
+<style lang="scss" scoped></style>
